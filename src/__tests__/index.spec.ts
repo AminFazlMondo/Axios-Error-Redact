895e18fdc6470573b5431fb35cb961517a89b86c
Amin Fazl <amin.fazl@mondo.com.au>
2021-06-28 23:55:07 +1000

    Request and query implementation

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,9 +1,89 @@
-import {AxiosErrorRedactor} from '../index'
-import axios from 'axios'
+import {AxiosErrorRedactor, redactedKeyword} from '../index'
+import axios, {AxiosError, AxiosResponse} from 'axios'
+import {expect} from 'chai'
 
 const redactor = new AxiosErrorRedactor()
 
-it('Invalid URL', async () => {
+function extractAxiosError(response: AxiosError | AxiosResponse | null | undefined): AxiosError {
+  if (response && 'isAxiosError' in response)
+    return response as AxiosError
+
+  throw new Error('Is not a Axios error')
+}
+
+function shouldNotContainRequestData(error: AxiosError): void {
+  expect(error.config.data).to.be.undefined
+  expect(error.request.data).to.be.undefined
+}
+
+function shouldRedactContainRequestData(error: AxiosError): void {
+  expect(error.config.data).to.be.equal(redactedKeyword)
+  expect(error.response?.config.data).to.be.equal(redactedKeyword)
+}
+
+it('Authorized with bearer token', async () => {
+  const auth = 'Bearer myToken'
+  const payload = {foo: 'bar'}
+  const response = await axios.put('Invalid-URL', payload, {
+    headers:{
+      Authorization: auth
+    }
+  }).catch(e => redactor.redactError(e))
+  const error = extractAxiosError(response)
+  shouldRedactContainRequestData(error)
+  expect(error.config.headers.Authorization).to.be.equal(redactedKeyword)
+  expect(error.request.connection._httpMessage._header).to.not.include(auth)
+})
+
+
+
+it('Basic Auth', async () => {
+  const username = 'stub-username'
+  const password = 'stub-password'
+  const response = await axios.get('Invalid-URL', {
+    auth: {
+      username,
+      password
+    }
+  }).catch(e => redactor.redactError(e))
+  const error = extractAxiosError(response)
+  expect(error.config.auth?.username).to.be.equal(redactedKeyword)
+  expect(error.config.auth?.password).to.be.equal(redactedKeyword)
+  expect(error.request.connection._httpMessage._header).to.not.include(username)
+  expect(error.request.connection._httpMessage._header).to.not.include(password)
+})
+
+it('No request body', async () => {
   const response = await axios.get('Invalid-URL').catch(e => redactor.redactError(e))
-  console.log('###DEBUG-response:', response)
+  const error = extractAxiosError(response)
+  shouldNotContainRequestData(error)
+})
+
+it('Request body', async () => {
+  const auth = 'Bearer myToken'
+  const payload = {foo: 'bar'}
+  const response = await axios.post('Invalid-URL', payload, {
+    headers:{
+      Authorization: auth
+    }
+  }).catch(e => redactor.redactError(e))
+  const error = extractAxiosError(response)
+  shouldRedactContainRequestData(error)
+})
+
+it('Query params', async () => {
+  const response = await axios.get('Invalid-URL?secret=mySecret').catch(e => redactor.redactError(e))
+  const error = extractAxiosError(response)
+  expect(error.config.url).to.be.equal('Invalid-URL?<REDACTED>')
+  expect(error.request.path).to.be.equal('Invalid-URL?<REDACTED>')
+  expect(error.request.res.responseUrl).to.include('Invalid-URL?<REDACTED>')
+})
+
+it('Fragment params', async () => {
+  const secret = 'stubSecret'
+  const response = await axios.get(`Invalid-URL#${secret}`).catch(e => redactor.redactError(e))
+  const error = extractAxiosError(response)
+  expect(error.config.url).to.be.equal('Invalid-URL#<REDACTED>')
+  expect(error.request.path).to.not.include(secret)
+  expect(error.request.res.responseUrl).to.not.include(secret)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,82 @@
-import {AxiosError} from 'axios'
+import { AxiosError } from 'axios'
+
+export const redactedKeyword = '<REDACTED>'
+
+function redactRequest(error: AxiosError): void {
+  const { request, config } = error
+  if (request?.data)
+    request.data = redactedKeyword
+
+  if (config.data)
+    config.data = redactedKeyword
+}
+
+const queryParamsRegex = /(?<=\?|#)\S+/ig
+function redactUrlQueryParams(url: string): string {
+  return url.replace(queryParamsRegex, redactedKeyword)
+}
+
+function redactQuery(error: AxiosError): void {
+  const { request, config } = error
+  if (config.url)
+    config.url = redactUrlQueryParams(config.url)
+
+  if (request.path)
+    request.path = redactUrlQueryParams(request.path)
+
+  if (request.res?.responseUrl)
+    request.res.responseUrl = redactUrlQueryParams(request.res.responseUrl)
+
+  if (request.res?._currentUrl)
+    request.res._currentUrl = redactUrlQueryParams(request.res._currentUrl)
+
+  if (request._redirectable?._currentUrl)
+    request._redirectable._currentUrl = redactUrlQueryParams(request._redirectable._currentUrl)
+
+  if (request.res?.req?._redirectable?._options?.search)
+    request.res.req._redirectable._options.search = redactUrlQueryParams(request.res.req._redirectable._options.search)
+
+  if (request.res?.req?._redirectable?._options?.path)
+    request.res.req._redirectable._options.path = redactUrlQueryParams(request.res.req._redirectable._options.path)
+
+  if (request._header)
+    request._header = redactUrlQueryParams(request._header)
+}
+
+const authorizationRegex = /(?<=Authorization:).+/ig
+function redactAuthorization(error: AxiosError): void {
+  const { config, request } = error
+  const { headers } = config
+
+  if (!headers)
+    return
+
+  if (headers.Authorization)
+    headers.Authorization = redactedKeyword
+
+  if (headers.authorization)
+    headers.authorization = redactedKeyword
+
+  if (headers.AUTHORIZATION)
+    headers.AUTHORIZATION = redactedKeyword
+
+  if (config.auth) {
+    config.auth.username = redactedKeyword
+    config.auth.password = redactedKeyword
+  }
+
+  const _header = request?.connection?._httpMessage?._header
+  if (_header)
+    request.connection._httpMessage._header = _header.replace(authorizationRegex, redactedKeyword)
+}
 
 export class AxiosErrorRedactor {
   redactRequestData: boolean
   redactResponseData: boolean
-  redactQueryDate: boolean
+  redactQueryData: boolean
 
-  constructor(redactRequestData = true, redactResponseData = true, redactQueryDate = true) {
-    this.redactQueryDate = redactQueryDate
+  constructor(redactRequestData = true, redactResponseData = true, redactQueryData = true) {
+    this.redactQueryData = redactQueryData
     this.redactRequestData = redactRequestData
     this.redactResponseData = redactResponseData
   }
@@ -22,12 +92,22 @@ export class AxiosErrorRedactor {
   }
 
   skipQueryData(): AxiosErrorRedactor {
-    this.redactQueryDate = false
+    this.redactQueryData = false
     return this
   }
 
   redactError(error: AxiosError | null | undefined): (AxiosError | null | undefined) {
     if (!error || !error.isAxiosError)
       return
+
+    redactAuthorization(error)
+
+    if (this.redactRequestData)
+      redactRequest(error)
+
+    if (this.redactQueryData)
+      redactQuery(error)
+
+    return error
   }
 }
