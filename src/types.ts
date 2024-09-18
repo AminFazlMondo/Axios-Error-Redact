e62df2cfdb80c079e1b14180e79da8cece08c4dd
Amin Fazl <62678026+AminFazlMondo@users.noreply.github.com>
2024-09-18 12:37:43 +1000

    updated the type documentation

--- src/types.ts
+++ src/types.ts
@@ -7,11 +7,13 @@ export interface AxiosErrorRedactorOptions {
 
   /**
    * whether to redact response data
+   * @default true
    */
   readonly redactResponseDataEnabled?: boolean;
 
   /**
    * whether to redact query data
+   * @default true
    */
   readonly redactQueryDataEnabled?: boolean;
 }
@@ -20,17 +22,60 @@ export interface AxiosErrorRedactorOptions {
  * Redacted response of Axios error
  */
 export interface HttpErrorResponse {
+  /**
+   * whether the response is redacted
+   */
+  readonly isErrorRedactedResponse: true;
+
+  /**
+   * error message
+   */
   readonly message: string;
+
+  /**
+   * full url of the request
+   */
   readonly fullURL: string;
+
+  /**
+   * response details
+   */
   readonly response: {
+
+    /**
+     * status code of the response
+     */
     readonly statusCode?: number;
+
+    /**
+     * status message of the response
+     */
     readonly statusMessage: string;
+
+    /**
+     * response data, redacted if set to true
+     */
     readonly data: unknown;
   };
   readonly request: {
+    /**
+     * base url of the request
+     */
     readonly baseURL: string;
+
+    /**
+     * path of the request
+     */
     readonly path: string;
+
+    /**
+     * method of the request
+     */
     readonly method: string;
+
+    /**
+     * request data, redacted if set to true
+     */
     readonly data: unknown;
   };
 }
